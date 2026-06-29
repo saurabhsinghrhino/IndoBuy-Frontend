5cf99f102959a2d9e5ab901c91cdeb0183a0c5f3
Saurabh singh <saurabhsingh941576@gmail.com>
2026-06-29 17:59:03 +0530

    fix: update Navbar import path in CheckoutPage component

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import Navbar from "./Navbar.jsx";
+import Navbar from "../components/Navbar";
 
 const CheckoutPage = () => {
   const navigate = useNavigate();
